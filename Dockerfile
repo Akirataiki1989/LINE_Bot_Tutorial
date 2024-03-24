f701a35ecc18ee3cedc0a9ddebd66b46af1dc548
guieunuch <guieunnuch@gmail.com>
2024-03-24 08:38:51 +0800

    update

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.8.6-buster
-
-WORKDIR /app
-
-ADD . /app
-
-RUN pip install -r requirements.txt
-ENV run_env=docker
-CMD gunicorn -c gunicorn.py -k uvicorn.workers.UvicornWorker main:app
--- vercel-fastapi-deployment-master/main.py
+++ vercel-fastapi-deployment-master/main.py
@@ -1,24 +1,10 @@
-
-import os
-
-import uvicorn
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from time import time
 from fastapi import FastAPI, __version__
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
-from line.urls import line_app
-from view import view
 
 app = FastAPI()
-
-templates = Jinja2Templates(directory="templates")
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
-# View
-app.include_router(view)
-# LINE Bot
-app.include_router(line_app)
 
 html = f"""
 <!DOCTYPE html>
@@ -44,16 +30,6 @@ html = f"""
 async def root():
     return HTMLResponse(html)
 
-if __name__ == "__main__":
-    # Local WSGI: Uvicorn
-    port = int(os.getenv("PORT", 8001))
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=port,
-        workers=4,
-        log_level="info",
-        access_log=True,
-        use_colors=True,
-        reload=True,
-    )
+@app.get('/ping')
+async def hello():
+    return {'res': 'pong', 'version': __version__, "time": time()}
